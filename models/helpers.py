- added default data paths to data class - added accuracy, precision and recall metrics to q-network compile function - fixed test + metric vizualization

models/helpers.py
@@ -32,18 +32,20 @@ def download_file(url:str, local_filename:str):
 def save_model(agent: Agent, model_path):
     if not os.path.exists(os.path.dirname(model_path)):
         os.makedirs(os.path.dirname(model_path))
-    # Rename the model
+    # Get the model summary and save it to a file
+    agent.model_network.model.save(model_path)
+    logging.info(f"Model '{agent.model_network.model_name}' saved in: {model_path}")
+    logging.info(f"Model summary:\n{get_model_summary(agent)}")
+
+def get_model_summary(model):
     stream = io.StringIO()
-    agent.model_network.model.summary(print_fn=lambda x: stream.write(x + "\n"))
+    if isinstance(model, Agent):
+        model.model_network.model.summary(print_fn=lambda x: stream.write(x + "\n"))
+    else:
+        model.summary(print_fn=lambda x: stream.write(x + "\n"))
     summary_str = stream.getvalue()
     stream.close()
-    agent.model_network.model.save(model_path)
-    logging.info("Model summary:\n{}".format(summary_str))
-    logging.info("Model saved in: {}".format(model_path))
-
-
-def load_model(agent: Agent, model_path):
-    agent.model_network.model.load_model(model_path)
+    return summary_str
 
 def download_datasets_if_missing(kdd_train:str, kdd_test:str):
     # If the data files for some reason do not exist, download them from the repo this work is based on.
@@ -61,12 +63,11 @@ def logger_setup():
     # Configure logging
     log_filename = os.path.join(cwd, 'logs/{}.log'.format(timestamp_begin))
     logging.basicConfig(filename=os.path.join(cwd, log_filename), level=logging.DEBUG,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-    
+                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
     # Create a console handler for the logger
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S'))
 
     # Add the console handler to the logger
     logging.getLogger().addHandler(console_handler)
@@ -74,3 +75,6 @@ def logger_setup():
     # Redirect TensorFlow logs to the logging module
     tf.get_logger().setLevel('INFO')
     tf.get_logger().addHandler(console_handler)
+
+    # Disable matplotlib logging
+    logging.getLogger('matplotlib').setLevel(logging.WARN)

models/q_network.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow.python.keras.losses import Huber
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras import optimizers
+from tensorflow.keras.metrics import Precision, Recall
 
 cwd = os.getcwd()
 models_dir = os.path.join(cwd, "models/trained-models/")
@@ -20,6 +21,7 @@ class QNetwork():
         """
         self.obs_size = obs_size
         self.num_actions = num_actions
+        self.model_name = model_name
 
         # Network arquitecture // TODO: add tf.keras.layers.Normalization() to normalize the input
         self.model = tf.keras.Sequential(name=model_name)
@@ -39,8 +41,10 @@ class QNetwork():
         optimizer = optimizers.Adam(learning_rate)
         # optimizer = optimizers.RMSpropGraves(learning_rate, 0.95, self.momentum, 1e-2)
 
+        #f1_score = F1Score() -> use this if you want to use the F1 score as a metric in the model. For my OS configs, this is not available.
         # Compilation of the model with optimizer and loss
-        self.model.compile(loss=Huber(delta=1.0), optimizer=optimizer)
+        self.model.compile(loss=Huber(delta=1.0), optimizer=optimizer,
+                            metrics=["accuracy", Precision(), Recall()])
 
     def predict(self, state, batch_size=1):
         """

utils.py
@@ -1,19 +1,18 @@
 
-import io
-import itertools
 import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import seaborn as sn
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.metrics import confusion_matrix
 import tensorflow as tf
 import time
 
 
-from models.attack_agent import AttackAgent
-from models.rl_env import RLenv
+from data.data_cls import DataCls
+from models.helpers import get_model_summary
 
 def plot_rewards_and_losses_during_training(def_reward_chain, att_reward_chain, def_loss_chain, att_loss_chain, path) -> plt.Figure:
     """
@@ -30,6 +29,11 @@ def plot_rewards_and_losses_during_training(def_reward_chain, att_reward_chain, 
     if not os.path.exists(path):
         os.makedirs(path)
     # Plot training results
+
+    # Create x-ticks based on the length of def_reward_chain
+    x_ticks = np.arange(len(def_reward_chain))
+    x_labels = [f'{i+1}' for i in range(len(def_reward_chain))]
+
     plt.figure(1)
     plt.subplot(211)
     plt.plot(np.arange(len(def_reward_chain)),def_reward_chain,label='Defense')
@@ -37,53 +41,64 @@ def plot_rewards_and_losses_during_training(def_reward_chain, att_reward_chain, 
     plt.title('Total reward by episode')
     plt.xlabel('n Episode')
     plt.ylabel('Total reward')
+    plt.xticks(x_ticks, x_labels)
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
         ncol=2, mode="expand", borderaxespad=0.)
 
     plt.subplot(212)
-    plt.plot(np.arange(len(def_loss_chain)),def_loss_chain,label='Defense')
-    plt.plot(np.arange(len(att_loss_chain)),att_loss_chain,label='Attack')
+    plt.plot(np.arange(len(def_loss_chain)), def_loss_chain,label='Defense')
+    plt.plot(np.arange(len(att_loss_chain)), att_loss_chain,label='Attack')
     plt.title('Loss by episode')
     plt.xlabel('n Episode')
     plt.ylabel('loss')
+    plt.xticks(x_ticks, x_labels)
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
         ncol=2, mode="expand", borderaxespad=0.)
     plt.tight_layout()
     plt.savefig(os.path.join(path, 'train_reward_loss.eps'), format='eps', dpi=1000)
-    
+    plt.savefig(os.path.join(path, 'train_reward_loss.pdf'), format='pdf', dpi=1000)
+    plt.close()
+
     return plt
 
 def plot_attack_distributions(attacks_by_epoch, attack_names, attack_labels_list, path) -> plt.Figure:
     bins=np.arange(23)
     # Plot attacks distribution alongside
     plt.figure(2,figsize=[12,5])
+    plt.xticks([])
+    plt.yticks([])
     plt.title("Attacks distribution throughout episodes")
-    #for indx,e in enumerate([0,70,90]):
-    for indx,e in enumerate([0]):
+    for indx,e in enumerate([0,70,90]):
+    #for indx,e in enumerate([0]):
         plt.subplot(1,3,indx+1)
         plt.hist(attacks_by_epoch[e], bins=bins, width=0.9, align='left')
         plt.xlabel("{} epoch".format(e))
         plt.xticks(bins, attack_names, rotation=90)
 
-
     plt.tight_layout()
     plt.savefig(os.path.join(path, 'attacks_distribution.eps'), format='eps', dpi=1000)
-
+    plt.savefig(os.path.join(path, 'attacks_distribution.pdf'), format='pdf', dpi=1000)
+    plt.close()
 
     # Plot attacks distribution alongside
     plt.figure(3,figsize=[10,10])
+    plt.xticks([])
+    plt.yticks([])
     plt.title("Attacks (mapped) distribution throughout  episodes")
-    #for indx,e in enumerate([0,10,20,30,40,60,70,80,90]):
-    for indx,e in enumerate([0]):
+    for indx,e in enumerate([0,10,20,30,40,60,70,80,90]):
+    #for indx,e in enumerate([0]):
         plt.subplot(3,3,indx+1)
         plt.bar(range(5), attack_labels_list[e], tick_label = ['Normal','Dos','Probe','R2L','U2R'])
         plt.xlabel("{} epoch".format(e))
+
     plt.tight_layout()
     plt.savefig(os.path.join(path, 'attacks_mapped_distribution.eps'), format='eps', dpi=1000)
+    plt.savefig(os.path.join(path, 'attacks_mapped_distribution.pdf'), format='pdf', dpi=1000)
+    plt.close()
 
     return plt
 
-def plot_confusion_matrix(cm, classes, #TODO: check why the saved file is faulty (graphics shadowwing over graphics)
+def plot_confusion_matrix(cm, classes,
                           path,
                           normalize=False,
                           title='Confusion matrix',
@@ -98,12 +113,27 @@ def plot_confusion_matrix(cm, classes, #TODO: check why the saved file is faulty
     else:
         logging.info('Confusion matrix, without normalization')
 
-    logging.info(f"\n{cm}")
 
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
+    logging.info(f"\nConfusion Matrix:\n{cm}")
+    plt.figure(figsize=(10,7))
+    sn.heatmap(cm, annot=True, fmt='g', xticklabels=classes, yticklabels=classes, cmap='Blues')
+    plt.xlabel('Predicted label')
+    plt.ylabel('True label')
+    plt.savefig(os.path.join(path, 'confusion_matrix.eps'), format='eps', dpi=1000)
+    plt.savefig(os.path.join(path, 'confusion_matrix.pdf'), format='pdf', dpi=1000)
+    plt.close()
+    #fig, ax = plt.subplots(figsize=(8, 6))  
+    #im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    #plt.imshow(cm, interpolation='nearest', cmap=cmap)
+
+    #ax.title(title)
+    #fig.colorbar(im)
+    #tick_marks = np.arange(len(classes))
+    #ax.set_xticks(tick_marks)
+    #ax.set_yticks(tick_marks)
+    #ax.set_xticklabels(classes, rotation=45)
+    #ax.set_yticklabels(classes)
+    """
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
 
@@ -118,98 +148,87 @@ def plot_confusion_matrix(cm, classes, #TODO: check why the saved file is faulty
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.savefig(os.path.join(path, 'confusion_matrix.eps'), format='eps', dpi=1000)
+    plt.close()
+    """
 
     return plt
 
-def test_trained_agent_quality(agent: AttackAgent, path_to_model, path_to_formated_test_data, plots_path):
+def test_trained_agent_quality(path_to_model, plots_path):
     model = tf.keras.models.load_model(path_to_model)
-    logging.info("Model loaded from: {}".format(path_to_model))
-    stream = io.StringIO()
-    agent.model_network.model.summary(print_fn=lambda x: stream.write(x + "\n"))
-    summary_str = stream.getvalue()
-    stream.close()
-    logging.info("Model summary:\n{}".format(summary_str))
-    #loss, acc = model.evaluate(test_images, test_labels, verbose=2) TODO: überprüfen ob das ausreicht anstelle der kleinschrittigen Berechnung unterhalb.
-    #logging.info('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
-    #logging.info(model.predict(test_images).shape)
-
-    #model.compile(loss=Huber(delta=1.0),optimizer=optimizers.Adam(0.00025)) # TODO: how can I read out the used optimizer from the loaded model? probably not even needed since the new .keras format
+    logging.info(f"Model '{model.name}' loaded from: {path_to_model}")
+    logging.info(f"Model summary:\n{get_model_summary(model)}")
 
     # Define environment, game, make sure the batch_size is the same in train
-    env_test = RLenv('test', agent, formated_test_path = path_to_formated_test_data)
+    test_data = DataCls(dataset_type='test')
 
     total_reward = 0
-    true_labels = np.zeros(len(env_test.attack_types),dtype=int)
-    estimated_labels = np.zeros(len(env_test.attack_types),dtype=int)
-    estimated_correct_labels = np.zeros(len(env_test.attack_types),dtype=int)
+    true_labels = np.zeros(len(test_data.attack_types),dtype=int)
+    predicted_labels = np.zeros(len(test_data.attack_types),dtype=int)
+    predicted_correct_labels = np.zeros(len(test_data.attack_types),dtype=int)
 
     #states , labels = env.get_sequential_batch(test_path,batch_size = env.batch_size)
-    states, labels = env_test.get_full()
-
+    states, labels = test_data.get_full() # get test data and true labels.
 
     start_time=time.time()
     q = model.predict(states)
-    actions = np.argmax(q,axis=1)
+    actions = np.argmax(q,axis=1) # get the action with the highest Q-value -> the predicted attack type
 
-    #
-    true_attack_type_indices=[]
+    true_attack_type_indices=[] # list of true attack types as indices (0-4). 0=normal, 1=dos, 2=probe, 3=r2l, 4=u2r. Length = number of samples
     for _, label in labels.iterrows():
         label_class = label.idxmax()
-        attack_type = env_test.attack_map[label_class]
-        attack_type_index = env_test.attack_types.index(attack_type)
+        attack_type = test_data.attack_map[label_class]
+        attack_type_index = test_data.attack_types.index(attack_type)
         true_attack_type_indices.append(attack_type_index)
 
-    labels, counts = np.unique(true_attack_type_indices, return_counts=True)
-    true_labels[labels] += counts
+    labels_per_attack_type, counts = np.unique(true_attack_type_indices, return_counts=True)
+    true_labels[labels_per_attack_type] += counts
 
     for indx, a in enumerate(actions):
-        estimated_labels[a] +=1
+        predicted_labels[a] +=1
         if a == true_attack_type_indices[indx]:
             total_reward += 1
-            estimated_correct_labels[a] += 1
+            predicted_correct_labels[a] += 1
 
-
-    action_dummies = pd.get_dummies(actions)
-    posible_actions = np.arange(len(env_test.attack_types))
-    for non_existing_action in posible_actions:
-        if non_existing_action not in action_dummies.columns:
-            action_dummies[non_existing_action] = np.uint8(0)
-    labels_dummies = pd.get_dummies(true_attack_type_indices)
-
-    f1_scores = calculate_unique_f1_scores_per_class(action_dummies, labels_dummies)
-
-
-    mismatch = estimated_labels - true_labels
-
+    f1_scores = calculate_unique_f1_scores_per_class(actions, test_data.attack_types, true_attack_type_indices)
+    mismatch = predicted_labels - true_labels
     acc = float(100 * total_reward / len(states))
-    logging.info('\nTotal reward: {} | Number of samples: {} | Accuracy = {:.2f}%'.format(total_reward,
-        len(states), acc))
+
+    logging.info(f"Overall overview\nTotal reward: {total_reward} | Number of samples: {len(states)} | Accuracy = {acc:.2f}%")
     
-    outputs_df = calculate_general_overview_per_attack_type(env_test.attack_types, estimated_labels, estimated_correct_labels, true_labels, f1_scores, mismatch)
-    logging.info(f"\n{outputs_df}")
+    outputs_df = calculate_general_overview_per_attack_type(test_data.attack_types, predicted_labels, predicted_correct_labels, true_labels, f1_scores, mismatch)
+    logging.info(f"Overall overview per attack type\n{outputs_df}")
 
     aggregated_data_test = np.array(true_attack_type_indices)
     print_aggregated_performance_measures(actions, aggregated_data_test)
 
-
     cnf_matrix = confusion_matrix(aggregated_data_test, actions)
     np.set_printoptions(precision=2)
-    plot_confusion_matrix(cnf_matrix, classes=env_test.attack_types, path=plots_path, normalize=True,
+    plot_confusion_matrix(cnf_matrix, classes=test_data.attack_types, path=plots_path, normalize=True,
                         title='Normalized confusion matrix')
-    plt.savefig(os.path.join(plots_path, 'confusion_matrix_adversarial.eps'), format='eps', dpi=1000)
 
     perf_per_class = calculate_one_vs_all_metrics(true_attack_type_indices, actions)
     logging.info("\r\nOne vs All metrics: \r\n{}".format(perf_per_class))
+    result = model.evaluate(states, pd.get_dummies(true_attack_type_indices), verbose=2)
+    loss, acc_model, precision, recall = result
+    logging.info(f"Model metrics: loss={loss}, accuracy={acc_model}, precision={precision}, recall={recall}")
+    logging.info(f"Time needed for testing: {time.time()-start_time}")
 
+def calculate_unique_f1_scores_per_class(predicted_actions, attack_types, true_labels):
+    predicted_actions_dummies = pd.get_dummies(predicted_actions)
+    posible_actions = np.arange(len(attack_types))
+    for non_existing_action in posible_actions:
+        if non_existing_action not in predicted_actions_dummies.columns:
+            predicted_actions_dummies[non_existing_action] = np.uint8(0)
+    true_labels_dummies = pd.get_dummies(true_labels)
 
-def calculate_unique_f1_scores_per_class(predicted_actions, true_labels):
-        normal_f1_score = f1_score(true_labels[0].values, predicted_actions[0].values)
-        dos_f1_score = f1_score(true_labels[1].values, predicted_actions[1].values)
-        probe_f1_score = f1_score(true_labels[2].values, predicted_actions[2].values)
-        r2l_f1_score = f1_score(true_labels[3].values, predicted_actions[3].values)
-        u2r_f1_score = f1_score(true_labels[4].values, predicted_actions[4].values)
+    normal_f1_score = f1_score(true_labels_dummies[0].values, predicted_actions_dummies[0].values)
+    dos_f1_score = f1_score(true_labels_dummies[1].values, predicted_actions_dummies[1].values)
+    probe_f1_score = f1_score(true_labels_dummies[2].values, predicted_actions_dummies[2].values)
+    r2l_f1_score = f1_score(true_labels_dummies[3].values, predicted_actions_dummies[3].values)
+    u2r_f1_score = f1_score(true_labels_dummies[4].values, predicted_actions_dummies[4].values)
+    overall_f1_score = f1_score(true_labels, predicted_actions, average='weighted')
 
-        return [normal_f1_score, dos_f1_score, probe_f1_score, r2l_f1_score, u2r_f1_score]
+    return [normal_f1_score, dos_f1_score, probe_f1_score, r2l_f1_score, u2r_f1_score, overall_f1_score]
 
 def print_aggregated_performance_measures(predicted_actions, true_labels):
     logging.info('Performance measures on Test data')
@@ -222,25 +241,27 @@ def calculate_one_vs_all_metrics(true_attack_type_indices, actions):
     mapa = {0:'normal', 1:'DoS', 2:'Probe',3:'R2L',4:'U2R'}
     yt_app = pd.Series(true_attack_type_indices).map(mapa)
 
-    perf_per_class = pd.DataFrame(index=range(len(yt_app.unique())),columns=['name', 'acc','f1', 'pre','rec'])
+    perf_per_class = pd.DataFrame(index=range(len(yt_app.unique())),columns=['name', 'accuracy','f1', 'precision','recall'])
     for i,x in enumerate(pd.Series(yt_app).value_counts().index):
         y_test_hat_check = pd.Series(actions).map(mapa).copy()
         y_test_hat_check[y_test_hat_check != x] = 'OTHER'
         yt_app = pd.Series(true_attack_type_indices).map(mapa).copy()
         yt_app[yt_app != x] = 'OTHER'
-        ac=accuracy_score( yt_app,y_test_hat_check)
-        f1=f1_score( yt_app,y_test_hat_check,pos_label=x, average='binary')
-        pr=precision_score( yt_app,y_test_hat_check,pos_label=x, average='binary')
-        re=recall_score( yt_app,y_test_hat_check,pos_label=x, average='binary')
+        ac=accuracy_score(yt_app, y_test_hat_check)
+        f1=f1_score(yt_app, y_test_hat_check, pos_label=x, average='binary')
+        pr=precision_score(yt_app, y_test_hat_check, pos_label=x, average='binary')
+        re=recall_score(yt_app, y_test_hat_check, pos_label=x, average='binary')
         perf_per_class.iloc[i]=[x,ac,f1,pr,re]
         
     return perf_per_class
 
-def calculate_general_overview_per_attack_type(attack_types, estimated_labels, estimated_correct_labels, true_labels, f1_scores, mismatch):
+def calculate_general_overview_per_attack_type(attack_types, estimated_labels, estimated_correct_labels, true_labels, f1_scores, mismatch) -> pd.DataFrame:
     outputs_df = pd.DataFrame(index = attack_types, columns = ["Estimated", "Correct", "Total", "F1_score", "Mismatch"])
-    for indx, att in enumerate(attack_types):
+    for indx, _ in enumerate(attack_types):
         outputs_df.iloc[indx].Estimated = estimated_labels[indx]
         outputs_df.iloc[indx].Correct = estimated_correct_labels[indx]
         outputs_df.iloc[indx].Total = true_labels[indx]
         outputs_df.iloc[indx].F1_score = f1_scores[indx]*100
         outputs_df.iloc[indx].Mismatch = abs(mismatch[indx])
+
+    return outputs_df